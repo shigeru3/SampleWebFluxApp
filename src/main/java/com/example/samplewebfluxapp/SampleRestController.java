e89908d8abb1c325e8eb9234d6496390c0bde18e
shigeru <hironoki3@gmail.com>
2023-07-14 17:41:26 +0900

    use file

--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -2,12 +2,17 @@ package com.example.samplewebfluxapp;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RestController
@@ -46,6 +51,24 @@ public class SampleRestController {
 	public Flux<Object> posts() {
 		List<Post> posts = repository.findAll();
 		return Flux.fromArray(posts.toArray());
+	}
+
+	@RequestMapping("/file")
+	public Mono<String> file() {
+		String result = "";
+		try {
+			ClassPathResource cr = new ClassPathResource("sample.txt");
+			InputStream is = cr.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+		} catch (IOException e) {
+			result = e.getMessage();
+		}
+		return Mono.just(result);
 	}
 
 	@PostConstruct
