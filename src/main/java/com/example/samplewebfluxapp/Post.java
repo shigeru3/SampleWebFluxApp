c0d2d1948c2db28a00ab9f802145416856a690fe
shigeru <hironoki3@gmail.com>
2023-07-14 17:14:09 +0900

    return entity object

---
+++ src/main/java/com/example/samplewebfluxapp/Post.java
@@ -0,0 +1,37 @@
+package com.example.samplewebfluxapp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="post")
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	public int id;
+
+	@Column
+	public int userId;
+
+	@Column(nullable = false)
+	public String title;
+
+	@Column(nullable = false)
+	public String body;
+
+	public Post() {
+		super();
+	}
+
+	public Post(int id, int userId, String title, String body) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.title = title;
+		this.body = body;
+	}
+
+	public String toString() {
+		return "{id:" + id + ", userId:" + userId + ", title:" + title + ", body:\"" + body + "\"}";
+	}
+}
--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -1,5 +1,6 @@
 package com.example.samplewebfluxapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -7,6 +8,9 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class SampleRestController {
+	@Autowired
+	PostRepository repository;
+
 	@RequestMapping("/")
 	public String hello() {
 		return "Hello Flux!";
@@ -20,5 +24,11 @@ public class SampleRestController {
 	@RequestMapping("/flux2")
 	public Flux<String> flux2() {
 		return Flux.just("Hello Flux", "This is sample of Flux");
+	}
+
+	@RequestMapping("/post")
+	public Mono<Post> post() {
+		Post post  = new Post(0, 0, "dummy", "dummy message");
+		return Mono.just(post);
 	}
 }
