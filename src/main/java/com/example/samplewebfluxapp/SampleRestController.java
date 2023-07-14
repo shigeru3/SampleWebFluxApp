4fde3c696385457ea92f66aac49a5fdcbed52b92
shigeru <hironoki3@gmail.com>
2023-07-14 16:58:27 +0900

    Hello Flux Mono

--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -2,11 +2,23 @@ package com.example.samplewebfluxapp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class SampleRestController {
 	@RequestMapping("/")
 	public String hello() {
 		return "Hello Flux!";
+	}
+
+	@RequestMapping("/flux")
+	public Mono<String> flux() {
+		return Mono.just("Hello Flux (Mono)");
+	}
+
+	@RequestMapping("/flux2")
+	public Flux<String> flux2() {
+		return Flux.just("Hello Flux", "This is sample of Flux");
 	}
 }
