baffdcdd42c2c2e88b4ca0d05ba0379853888ede
shigeru <hironoki3@gmail.com>
2023-07-14 17:19:43 +0900

    return entity object from database

--- src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -1,10 +1,14 @@
 package com.example.samplewebfluxapp;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class SampleRestController {
@@ -30,5 +34,27 @@ public class SampleRestController {
 	public Mono<Post> post() {
 		Post post  = new Post(0, 0, "dummy", "dummy message");
 		return Mono.just(post);
+	}
+
+	@RequestMapping("/post/{id}")
+	public Mono<Post> post(@PathVariable int id) {
+		Post post = repository.findById(id);
+		return Mono.just(post);
+	}
+
+	@RequestMapping("/posts")
+	public Flux<Object> posts() {
+		List<Post> posts = repository.findAll();
+		return Flux.fromArray(posts.toArray());
+	}
+
+	@PostConstruct
+	public void init() {
+		Post p1 = new Post(1, 1, "Hello", "Hello Flux");
+		Post p2 = new Post(2, 2, "Sample", "This is sample");
+		Post p3 = new Post(3, 3, "Hi", "This is post");
+		repository.saveAndFlush(p1);
+		repository.saveAndFlush(p2);
+		repository.saveAndFlush(p3);
 	}
 }
